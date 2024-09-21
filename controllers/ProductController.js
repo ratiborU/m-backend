b090377c27120f584e46dfab90cc7b25dd39ff87
ratiborU <tvinck_444@mail.ru>
2024-09-21 11:33:47 +0500

    refactor: product service

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,74 +1,55 @@
 import { Product } from "../models/models.js";
 import ApiError from "../error/ApiError.js"; 
 
+import ProductService from "../services/ProductService.js";
+
 class ProductController {
-    async create(req, res) {
-        const { title, description, characteristics, price, rate, commentsCount } = req.body;
-        const product = await Product.create({title, description, characteristics, price, rate, commentsCount})
-        return res.json(product);
+    async create(req, res, next) {
+        try {
+            const product = await ProductService.create(req.body);
+            return res.json(product);
+        } catch (error) {
+            next(error);
+        }
     }
 
-    async getAll(req, res) {
-        let { limit, page } = req.query;
-        page = page || 1;
-        limit = limit || 16;
-        let offset = (page - 1) * limit;
-        const products = await Product.findAndCountAll({limit: limit, offset: offset});
-        return res.json(products);
+    async getAll(req, res, next) {
+        try {
+            let { limit, page } = req.query;
+            const products = await ProductService.getAll(limit, page);
+            return res.json(products);
+        } catch (error) {
+            next(error);
+        }
     }
 
-    async getOne(req, res) {
-        const {id} = req.params;
-        const product = await Product.findByPk(id);
-        return res.json(product);
+    async getOne(req, res, next) {
+        try {
+            const {id} = req.params;
+            const product = await ProductService.getOne(id);
+            return res.json(product);
+        } catch (error) {
+            next(error);
+        }
     }
 
-    async update(req, res) {
-        const { id, title, description, characteristics, price, rate, commentsCount } = req.body;
-        const product = await Product.update({
-            title, description, characteristics, price, rate, commentsCount
-        }, {
-            where: { id: id }
-        });
-        return res.json(product);
+    async update(req, res, next) {
+        try {
+            const product = await ProductService.update(req.body);
+            return res.json(product);
+        } catch (error) {
+            next(error);
+        }
     }
 
-    async addRate(req, res) {
-        const { id, rate } = req.query;
-        const product = await Product.findByPk(id);
-        const updatedRate = (product.dataValues.rate * product.dataValues.commentsCount + Number(rate)) / (product.dataValues.commentsCount + 1)
-        
-        await Product.update({
-            rate: updatedRate, 
-            commentsCount: product.dataValues.commentsCount + 1
-        }, {
-            where: { id: id }
-        });
-        const updatedProduct = await Product.findByPk(id);
-        return res.json(updatedProduct);
-    }
-
-    async removeRate(req, res) {
-        const { id, rate } = req.query;
-        const product = await Product.findByPk(id);
-        const updatedRate = (product.dataValues.rate * product.dataValues.commentsCount - Number(rate)) / (product.dataValues.commentsCount - 1)
-
-        await Product.update({
-            rate: updatedRate, 
-            commentsCount: product.dataValues.commentsCount - 1
-        }, {
-            where: { id: id }
-        });
-        const updatedProduct = await Product.findByPk(id);
-        return res.json(updatedProduct);
-    }
-
-    async delete(req, res) {
-        const {id} = req.params;
-        const product = await Product.destroy({
-            where: { id: id }
-        });
-        return res.status(200).json(product);
+    async delete(req, res, next) {
+        try {
+            const {id} = req.params;
+            const product = await ProductService.delete(id);
+            return res.json(product);
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
